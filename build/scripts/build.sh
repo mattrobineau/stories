45b799b40fc7f919ca36cf6046dd18d8d3b1157d
Matt Robineau <matthieu.robineau@gmail.com>
2017-10-29 23:30:29 -0400

    Attempt Travis CI fix for gulp

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-npm install -g gulp
-npm link gulp
+# npm install -g gulp
+# npm link gulp
+# npm install
+dotnet restore
 npm install
-dotnet restore && dotnet build ./src/Stories/Stories.csproj -v d
+dotnet build ./src/Stories/Stories.csproj -v d
